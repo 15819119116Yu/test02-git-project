cd5931c19ab40781f0bf9e44f25be731c72f51fe
Layne <3356642156@qq.com>
2023-04-05 23:11:16 +0800

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * ClassName: ${NAME}
+ * Package: ${PACKGE_NAME}
+ * Descriptption:
+ *
+ * @Author: 吸血的小金鱼
+ * @Create: ${DATE} - ${TIME}
+ * @Version: v1.0
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
